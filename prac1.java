d4432bb54cc7155690fda5eb73719d7e86e45cf7
kakashi989 <maheshvetale999@gmail.com>
2020-07-16 10:00:47 +0530

    change in master

--- prac1.java
+++ prac1.java
@@ -2,8 +2,8 @@ class Prac
 {
 	public static void main(String xx[])
 	{
-		System.out.println("From branch newbranch");
+		System.out.println("From branch Master");
 		System.out.println("Hello World HIIIIIIII");
-		System.out.println("Hello World HIIIIIIII");
+		System.out.println("Hello World Byeeeeeeeeeeeeee);
 	}
 }
