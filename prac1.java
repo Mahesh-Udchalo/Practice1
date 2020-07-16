632e22db8b28cd96c58d27d4f8adc311cd4d20b7
kakashi989 <maheshvetale999@gmail.com>
2020-07-16 08:43:18 +0530

    Branch change 4

--- prac1.java
+++ prac1.java
@@ -2,11 +2,8 @@ class Prac
 {
 	public static void main(String xx[])
 	{
-		System.out.println("From branch 1");
-
-		System.out.println("From Branch 2");
+		System.out.println("From branch newbranch");
 		System.out.println("Hello World HIIIIIIII");
-
 		System.out.println("Hello World HIIIIIIII");
 	}
 }
